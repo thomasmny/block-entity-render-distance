3d001493a833bf1c39ba934eff7612f6b0b2759f
Thomas Meaney <thomas.meaney@icloud.com>
2022-08-19 14:10:41 +0200

    Switch to `cloth-config` (#8)

--- src/main/java/com/trichtern/blockentityrenderdistance/mixin/BlockEntityRendererMixin.java
+++ src/main/java/com/trichtern/blockentityrenderdistance/mixin/BlockEntityRendererMixin.java
@@ -1,6 +1,6 @@
 package com.trichtern.blockentityrenderdistance.mixin;
 
-import com.trichtern.blockentityrenderdistance.config.ModConfigs;
+import com.trichtern.blockentityrenderdistance.config.ModConfig;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,9 +15,6 @@ public interface BlockEntityRendererMixin {
 
     @Inject(method = "getRenderDistance", at = @At("RETURN"), cancellable = true)
     default void modifyRenderDistance(CallbackInfoReturnable<Integer> cir) {
-        final int defaultValue = cir.getReturnValueI();
-        final double modifier = ModConfigs.RENDER_DISTANCE_MODIFIER;
-
-        cir.setReturnValue((int) (defaultValue * modifier));
+        cir.setReturnValue(ModConfig.INSTANCE.renderDistance);
     }
 }
